85c6e366d859d6958639240082a047de1f153758
Joshua Kaluski <joshua@kaluski.com>
2019-07-31 21:09:18 -0400

    Created chatrooms for 5 different fandoms

---
+++ config/chatrooms.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    name: 'Doctor Who',
+    image: 'client/public/chatrooms/doctorWho.jpg'
+  },
+  {
+    name: 'Harry Potter',
+    image: 'client/public/chatrooms/harryPotter.jpg'
+  },
+  {
+    name: 'Marvel',
+    image: 'client/public/chatrooms/marvel.jpg'
+  },
+  {
+    name: 'Star Trek',
+    image: 'client/public/chatrooms/starTrek.jpg'
+  },
+  {
+    name: 'Star Wars',
+    image: 'client/public/chatrooms/starWars.jpg'
+  },
+];
