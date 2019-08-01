175c8d5677373c7971c7d9ed23e38bef384ab029
Joshua Kaluski <joshua@kaluski.com>
2019-08-01 16:54:41 -0400

    Created ChatroomManager

---
+++ server/ChatroomManager.js
@@ -0,0 +1,30 @@
+const Chatroom = require('./Chatroom');
+const ChatroomTemplates = require('../config/chatrooms');
+
+module.exports = () => {
+  //Mapping all chatrooms
+  const chatrooms = new Map(
+    ChatroomTemplates.map(chatroom => [
+      chatroom.name,
+      Chatroom(chatroom)
+    ])
+  );
+
+  function getChatroom(name) {
+    chatrooms.get(name)
+  }
+  
+  function removeClient(client) {
+    chatrooms.forEach(chatroom => chatroom.removeClient(client))
+  }
+
+  function serialize() {
+    return Array.from(chatrooms.values().map(chatroom => chatroom.getChatroomInfo()))
+  }
+
+  return {
+    getChatroom,
+    removeClient,
+    serialize
+  }
+};
