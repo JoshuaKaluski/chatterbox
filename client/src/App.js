d5677a584d719514e36317235279ad56b3075514
Joshua Kaluski <joshua@kaluski.com>
2019-08-13 08:58:00 -0400

    Started client logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,65 @@
-import React from 'react';
-import {Switch, Route} from 'react-router-dom';
-import SocketContext from './SocketContext';
-import io from 'socket.io-client'
-import Chat from './Chat/Chat'
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 
-const ENDPOINT = 'http://localhost:5000';
-
-const socket = io(ENDPOINT);
+import Landing from './Landing';
+import socket from './socket';
 
 function App() {
+  //State
+  const [state, setState] = useState({
+    user: null,
+    registerInProcess: false,
+    client: socket(),
+    chatrooms: null
+  });
+
+  //Get all chatrooms when first loaded
+  useEffect(() => getChatrooms(), []);
+
+  const onEnterChatroom = (chatroomName, noUser, success) => {
+    if (!state.user) {
+      return noUser();
+    }
+
+    return state.client.join(chatroomName, (err, chatLog) => {
+      if (err) return console.error(err);
+
+      return success(chatLog);
+    })
+  };
+
+  const onLeaveChatroom = (chatroomName, success) => {
+    state.client.leave(chatroomName, err => {
+      if (err) return console.error(err);
+      return success();
+    })
+  };
+
+  const getChatrooms = () => state.client.getChatrooms((err, chatrooms) => setState({...state, chatrooms}));
+
+  const register = (name, avatar) => {
+    //Reset registerInProcess to false
+    const registerResponse = user => setState({...state, registerInProcess: false, user});
+
+    //Register flow
+    setState({...state, registerInProcess: true});
+    state.client.register(name, avatar, (err, user) => {
+      if (err) return registerResponse(null);
+      return registerResponse(user);
+    })
+  };
 
   return (
-    <SocketContext.Provider value={socket}>
-      <div className="App">
-        <h1>Welcome</h1>
-        <Switch>
-          <Route exact to='/' component={Chat}/>
-        </Switch>
-      </div>
-    </SocketContext.Provider>
+    <MuiThemeProvider>
+      <Switch>
+        <Route exact path='/' render={props => (
+          <Landing
+            chatrooms={state.chatrooms}
+          />
+        )}/>
+      </Switch>
+    </MuiThemeProvider>
   );
 }
 
